e3397a4c8413fe8e87e5e2fc2d8b529f877846b8
SharonChan1 <34252442+SharonChan1@users.noreply.github.com>
2018-04-09 15:31:03 -0400

    Update BLE_controller.ino
    add motor demo function

--- Bluetooth/BLE_controller.ino
+++ Bluetooth/BLE_controller.ino
@@ -1,12 +1,9 @@
 /*********************************************************************
  This is an example for our nRF51822 based Bluefruit LE modules
-
  Pick one up today in the adafruit shop!
-
  Adafruit invests time and resources providing this open source code,
  please support Adafruit and open-source hardware by purchasing
  products from Adafruit!
-
  MIT license, check LICENSE for more information
  All text above, and the splash screen below must be included in
  any redistribution
@@ -24,16 +21,124 @@
 #if SOFTWARE_SERIAL_AVAILABLE
   #include <SoftwareSerial.h>
 #endif
- 
-#define FACTORYRESET_ENABLE         1
-#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-#define MODE_LED_BEHAVIOUR          "MODE"
 
+#include <Adafruit_MotorShield.h>
+#include <Wire.h>
+
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
+
+/*=========================================================================
+    APPLICATION SETTINGS
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         0
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+/*=========================================================================*/
 const int buzzerPin = 3;
 const int ledPin1 = 12;
 const int ledPin2 = 13;
 const int c = 261;
 
+// TONES  ==========================================
+// Start by defining the relationship between 
+//       note, period, &  frequency. 
+#define  c     3830    // 261 Hz 
+#define  d     3400    // 294 Hz 
+#define  e     3038    // 329 Hz 
+#define  f     2864    // 349 Hz 
+#define  g     2550    // 392 Hz 
+#define  a     2272    // 440 Hz 
+#define  b     2028    // 493 Hz 
+#define  C     1912    // 523 Hz 
+// Define a special note, 'R', to represent a rest
+#define  R     0
+
+// SETUP ============================================
+// Set up speaker on a PWM pin (digital 9, 10 or 11)
+int speakerOut = 9;
+// Do we want debugging on serial out? 1 for yes, 0 for no
+int DEBUG = 0;
+
+// MELODY and TIMING  =======================================
+//  melody[] is an array of notes, accompanied by beats[], 
+//  which sets each note's relative length (higher #, longer note) 
+int melody[] = {  C,  b,  g,  C,  b,   e,  R,  C,  c,  g, a, C };
+int beats[]  = { 16, 16, 16,  8,  8,  16, 32, 16, 16, 16, 8, 8 }; 
+int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping.
+
+// Set overall tempo
+long tempo = 10000;
+// Set length of pause between notes
+int pause = 1000;
+// Loop variable to increase Rest length
+int rest_count = 100; //<-BLETCHEROUS HACK; See NOTES
+
+// Initialize core variables
+int tone_ = 0;
+int beat = 0;
+long duration  = 0;
+
+// PLAY TONE  ==============================================
+// Pulse the speaker to play a tone for a particular duration
+void playTone() {
+  long elapsed_time = 0;
+  if (tone_ > 0) { // if this isn't a Rest beat, while the tone has 
+    //  played less long than 'duration', pulse speaker HIGH and LOW
+    while (elapsed_time < duration) {
+
+      digitalWrite(speakerOut,HIGH);
+      delayMicroseconds(tone_ / 2);
+
+      // DOWN
+      digitalWrite(speakerOut, LOW);
+      delayMicroseconds(tone_ / 2);
+
+      // Keep track of how long we pulsed
+      elapsed_time += (tone_);
+    } 
+  }
+  else { // Rest beat; loop times delay
+    for (int j = 0; j < rest_count; j++) { // See NOTE on rest_count
+      delayMicroseconds(duration);  
+    }                                
+  }                                 
+}
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 // A small helper
@@ -47,6 +152,8 @@ uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
 float parsefloat(uint8_t *buffer);
 void printHex(const uint8_t * data, const uint32_t numBytes);
 
+//function prototypes for soundbuzzer.cpp
+
 // the packet buffer
 extern uint8_t packetbuffer[];
 
@@ -57,25 +164,34 @@ extern uint8_t packetbuffer[];
             automatically on startup)
 */
 /**************************************************************************/
-void setup(void)
-{
+void setup(void) {  
+ // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    
   //Setup pin modes
-  pinMode(buzzerPin, OUTPUT);
+  pinMode(speakerOut, OUTPUT);
+  //pinMode(buzzerPin, OUTPUT);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
-  
-  while (!Serial);  // required for Flora & Micro
-  delay(500);
 
   Serial.begin(115200);
-  Serial.println(F("Ballbot Bluetooth Connection"));
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
   
+  Serial.println(F("Ballbot Bluetooth Connection"));
+  Serial.println(F("----------------------------"));
+
   /* Initialise the module */
   Serial.print(F("Initialising the Bluefruit LE module: "));
 
   if ( !ble.begin(VERBOSE_MODE) )
   {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+    error(F("Couldn't find Bluefruit, make sure it's in Command mode & check wiring?"));
   }
   Serial.println( F("OK!") );
 
@@ -93,9 +209,13 @@ void setup(void)
 
   ble.verbose(false);  // debug info is a little annoying after this point!
 
+  SPI.begin();
+  ble.begin();
+
   /* Wait for connection */
   while (! ble.isConnected()) {
       delay(500);
+      Serial.println("Connecting...");
   }
 
   Serial.println(F("******************************"));
@@ -113,6 +233,14 @@ void setup(void)
   ble.setMode(BLUEFRUIT_MODE_DATA);
 
   Serial.println(F("******************************"));
+
+  // Motor Shield: create with the default frequency 1.6KHz
+  AFMS.begin(); 
+
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  motor1->setSpeed(255);
+  motor2->setSpeed(255);
+  motor3->setSpeed(255);
 
 }
  
@@ -134,22 +262,49 @@ void loop(void)
     
     if(pressed & (buttnum == 1)) //sound buzzer
     {
-      tone(buzzerPin, c);
+      motor1->run(RELEASE);
+      motor2->run(RELEASE);
+      motor3->run(RELEASE);
+  // Set up a counter to pull from melody[] and beats[]
+  for (int i=0; i<MAX_COUNT; i++) {
+    tone_ = melody[i];
+    beat = beats[i];
+    duration = beat * tempo; // Set up timing
+    playTone(); 
+    // A pause between notes...
+    delayMicroseconds(pause);
+  } 
+      }
+    else if(pressed & (buttnum == 2)) //(all motors)
+    {
+      motor1->run(FORWARD);
+      motor2->run(BACKWARD);
+      motor3->run(BACKWARD);
+    }
+    else if(pressed & (buttnum == 5)) //Up
+    {
+      motor2->run(BACKWARD);
+      motor3->run(FORWARD);
+      motor1->run(RELEASE);
+    }
+    else if(pressed & (buttnum == 7)) //Left
+    {
+      motor1->run(FORWARD);
+      motor2->run(FORWARD);
+      motor3->run(RELEASE);
+    }
+    else if(pressed & (buttnum == 8)) //Right
+    {
+      motor1->run(BACKWARD);
+      motor3->run(BACKWARD);
+      motor2->run(RELEASE);
     }
     else if(!pressed)
     { 
       noTone(buzzerPin);
     }
-
-    if(pressed & (buttnum == 5)) //move ball clockwise
-    {
-      //move wheels 
-    }
-    if(pressed & buttnum == 6)) //move ball counterclockwise
-    {
-      //move wheels 
-    }
+    Serial.print("Button Pressed:  ");
+    Serial.println(buttnum);
   }
 
 }
-
